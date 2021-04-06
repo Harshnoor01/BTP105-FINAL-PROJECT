475470e73f3807625f1b4473089ee90282ff4b50
Harshnoor01 <81935079+Harshnoor01@users.noreply.github.com>
2021-04-06 16:52:55 -0400

    Update BTP105 V1 manipulating.h

--- BTP105 V1 manipulating.h
+++ BTP105 V1 manipulating.h
@@ -1,11 +1,15 @@
-#pragma once
+/* Name :- Harshnoor Sandhu
+Student ID :- 118502202
+Course Name :- BTP105NAA
+*/
+
 #ifndef _MANIPULATING_H
 #define _MANIPULATING_H
 
-#include <stdio.h>
-#include <string.h>
+#include <stdio.h>  //includes header file
+#include <string.h> //used to calculate length of a string 
 
-#define TRUE 1
+#define TRUE 1      // daclaration of glabal constant 
 void manipulating();
 
 #endif // !_MANIPULATING_H
