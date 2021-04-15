ad95eb2575b788a469b5350a72a59042a8860e50
Harshnoor01 <81935079+Harshnoor01@users.noreply.github.com>
2021-04-15 11:45:52 -0400

    Update V1 manipulating.c

--- V1 manipulating.c
+++ V1 manipulating.c
@@ -71,3 +71,27 @@ void manipulating()
 }
 
         
+//	VERSION #3 CODES
+
+	printf("*** Start of Searching Strings Demo ***\n");                        //starting message of searching module
+
+	while (TRUE)                                                                // entry point to loop
+	{
+		printf("Type the big string (q - to quit):\n");
+		gets(big_string);
+
+		if (strcmp(big_string, "q") == 0)
+			break;
+
+		printf("Type the substring:\n");                                //prints the message "Type the substring"
+		gets(sub_string);                                              // Gets user input and assigns it to variable string1.
+
+		address = strstr(big_string, sub_string);
+
+		if (address != NULL)                                             // If the condition is set to be true then it will exit the loop
+			printf("Found at %ld position\n", (long)address - (long)big_string);
+		else
+			printf("Not found\n");
+	}
+	printf("*** End of Searching Strings Demo ***\n\n");                  //Ending message of searching string           
+}
